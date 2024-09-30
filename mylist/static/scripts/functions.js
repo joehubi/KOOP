0bbacbddefb05625f076198a75f124b99dbd6758
joehubi <joehubi@gmail.com>
2024-09-30 10:59:19 +0200

    comments and chapters added

--- mylist/static/scripts/functions.js
+++ mylist/static/scripts/functions.js
@@ -1,35 +1,13 @@
 // functions.js
 
+//#region ######### Funktionen für HTML in Java Script
+
+// Funktion für die Pfaderstellung z.B. "Person_1/add"
 function generatePersonPath(personNumber, action) {
     return `/Person_${personNumber}/${action}/`; // erstellt den Pfad für die folgenden Funktionen (z.B. "Person_1/add/")
 }
 
-function addItemManually(personNumber) {
-    let itemName = prompt('Name des Artikels')
-    if (itemName === null) {
-        return;
-    }
-    let itemAmount = prompt('Menge (kg oder Stück)')
-    if (itemAmount === null) {
-        return;
-    }
-    let itemPrice = prompt('Preis in €')
-    if (itemPrice === null) {
-        return;
-    }
-  
-    let formData = new FormData();
-    formData.append('itemName', itemName);
-    formData.append('itemAmount', itemAmount);
-    formData.append('itemPrice', itemPrice);            
-    formData.append('csrfmiddlewaretoken', token);
-
-    fetch(generatePersonPath(personNumber, 'add'),{
-        method: 'POST',
-        body: formData
-    });
-}
-
+// Hinzufügen von Artikeln/Einkäufen
 function addItemManually2(personNumber) {
     var itemName = document.getElementById("artikel_manual").value;
     var itemAmount = document.getElementById("stk_kg_manual").value;
@@ -47,6 +25,39 @@ function addItemManually2(personNumber) {
     });
 }
 
+// Löschen eines Artikels/Einkaufs
+function deleteItem(personNumber) {
+
+    let itemNumber = prompt('Nummer des Einkaufs der gelöscht werden soll')
+    if (itemNumber === null) {
+        return;
+    }
+
+    let formData = new FormData();
+    formData.append('itemNumber', itemNumber);
+    formData.append('csrfmiddlewaretoken', token);
+
+    fetch(generatePersonPath(personNumber, 'delete'),{
+        method: 'POST',
+        body: formData
+    });
+}
+
+// Bilden der Summe des aktuellen Einkaufs
+function sum(personNumber) {
+
+    let formData = new FormData();
+    formData.append('csrfmiddlewaretoken', token);
+
+    fetch(generatePersonPath(personNumber, 'sum'),{
+        method: 'POST',
+        body: formData
+    });
+}
+//#endregion
+
+
+//#region ########### Aktuell NICHT aktiv
 function addItemAutomatic(personNumber) {
     let itemNumber = prompt('Nummer aus Preisliste auswählen')
     if (itemNumber === null) {
@@ -83,30 +94,31 @@ function addItemAutomatic2(personNumber) {
     });
 }
 
-function deleteItem(personNumber) {
-
-    let itemNumber = prompt('Nummer des Einkaufs der gelöscht werden soll')
-    if (itemNumber === null) {
+// with prompt
+function addItemManually(personNumber) {
+    let itemName = prompt('Name des Artikels')
+    if (itemName === null) {
         return;
     }
-
+    let itemAmount = prompt('Menge (kg oder Stück)')
+    if (itemAmount === null) {
+        return;
+    }
+    let itemPrice = prompt('Preis in €')
+    if (itemPrice === null) {
+        return;
+    }
+  
     let formData = new FormData();
-    formData.append('itemNumber', itemNumber);
+    formData.append('itemName', itemName);
+    formData.append('itemAmount', itemAmount);
+    formData.append('itemPrice', itemPrice);            
     formData.append('csrfmiddlewaretoken', token);
 
-    fetch(generatePersonPath(personNumber, 'delete'),{
+    fetch(generatePersonPath(personNumber, 'add'),{
         method: 'POST',
         body: formData
     });
 }
+//#endregion
 
-function sum(personNumber) {
-
-    let formData = new FormData();
-    formData.append('csrfmiddlewaretoken', token);
-
-    fetch(generatePersonPath(personNumber, 'sum'),{
-        method: 'POST',
-        body: formData
-    });
-}
