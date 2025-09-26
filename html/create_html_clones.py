39e1661d60f32140bcf4baae956eb956357acfe3
Johannes Huber <50733690+joehubi@users.noreply.github.com>
2025-09-26 21:06:13 +0200

    bugfix: koop not Koop

--- html/create_html_clones.py
+++ html/create_html_clones.py
@@ -9,6 +9,6 @@ base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 template_dir = os.path.join(base_dir, "mylist", "templates")
 
 for n in range(1, 16):
-    dest_file = os.path.join(template_dir, f"Koop_{n}.html")
+    dest_file = os.path.join(template_dir, f"koop_{n}.html")
     shutil.copyfile(source_file, dest_file)
     print(f"Kopiert: Koop_n.html -> Koop_{n}.html")
